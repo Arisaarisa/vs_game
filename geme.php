e5c0086d714c0616f5afe9d1171d73d98d517727
arisa <arisankochira10@gmail.com>
2021-03-05 10:11:23 +0900

    攻撃技の表示

--- geme.php
+++ geme.php
@@ -1,19 +1,32 @@
 <?php
-$hp = 20000;
+$hp = 10000;
+function attack($attack,$hp){
+}
 while ($hp > 0) {
+    $technique = [1 => "パンチ", 2 => "キック", 3 => "投げ技", 4 => "気功", 5 => "爆弾", 6 => "投げキッス"];
     echo "攻撃技は？" . PHP_EOL . "1.パンチ 2.キック 3.投げ技 4.気功 5.爆弾 6.投げキッス" . PHP_EOL;
     $attack = trim(fgets(STDIN));
     switch ($attack) {
         case 1:
-                // echo "パンチの攻撃！";
+                if ($attack = trim($attack) == 1) {
+                    echo $technique[1] . "の攻撃!!" . PHP_EOL;
+                }
         case 2:
-                // echo "キックの攻撃！";
+                if (trim($attack) == 2) {
+                    echo $technique[2] . "の攻撃!!" . PHP_EOL;
+                }
         case 3:
-                // echo "投げ技の攻撃！";
+                if (trim($attack) == 2) {
+                    echo $technique[2] . "の攻撃!!" . PHP_EOL;
+                }
         case 4:
-                // echo "気功の攻撃！";
+                if (trim($attack) == 2) {
+                    echo $technique[2] . "の攻撃!!" . PHP_EOL;
+                }
         case 5:
-                // echo "爆弾の攻撃！";
+                if (trim($attack) == 2) {
+                    echo $technique[2] . "の攻撃!!" . PHP_EOL;
+                }
 
             $attack = rand(500, 3000);
             if ($attack >= 2000) {
@@ -24,7 +37,7 @@ while ($hp > 0) {
             } else {
                 $hp = 0;
             }
-            echo "攻撃力" . $attack . " の攻撃！" . PHP_EOL . "HP: " . $hp . PHP_EOL;
+            echo  "攻撃力" . $attack . " の攻撃！" . PHP_EOL . "HP: " . $hp . PHP_EOL . PHP_EOL;
             break;
 
         case 6:
