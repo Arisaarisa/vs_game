d30085b2398688cb4d095bdabf64d5c463104210
arisa <arisankochira10@gmail.com>
2021-03-06 13:04:56 +0900

    スタートページの作成

--- start.php
+++ start.php
@@ -3,14 +3,18 @@
     <head>
         <meta charset="utf-8">
         <link rel="stylesheet" href="css/style.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
         <title>VS-GAME</title>
     </head>
     <body>
-        <div id="start_page">
-            <h1 id="start_text">Fighting GAME</h1>
-            <div id="start_btn">
-                <a href="hp_set.php">
-                    <button type="submit" class="click" id="start_the_match">対戦開始</button>
+        <div id="home" class="home_bg">
+            <div class="home_content wrapper">
+                <h1 class="home_title">Fighting GAME</h1>
+                <a href="hp_set.php" class="button" id="start_btn">
+                        対戦開始
                 </a>
             </div>
         </div>
